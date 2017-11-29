efed59f6e76bd021b8bd528d6ee4a197c34d021f
Paul Shryock <hi@pshry.com>
2017-11-29 08:12:14 -0500

    Include partials

--- header.php
+++ header.php
@@ -1,15 +1,19 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" <?php language_attributes(); ?>>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<?php wp_head(); ?>
+
 		<title>Marketing Consultant From New Jersey | Paul Shryock</title>
 		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/reset.css">
 		<link rel="stylesheet" href="css/style.css">
+
 	</head>
-	<body class="site">
+	<body <?php body_class( 'site' ); ?>>
 		<header class="site-header" role="banner">
 			<h1 class="header-logo"><a href="/">Paul Shryock</a></h1>
 			<nav class="header-nav">
