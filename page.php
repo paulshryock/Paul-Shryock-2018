fd99c07f3cc199597afb3794f3bc31437140ff82
Paul Shryock <hi@pshry.com>
2017-12-05 15:01:34 -0500

    Update page markup

--- page.php
+++ page.php
@@ -14,8 +14,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<section class="main-content">
 
 			<?php
 			while ( have_posts() ) : the_post();
@@ -30,9 +29,8 @@ get_header(); ?>
 			endwhile; // End of the loop.
 			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</section><!-- .main-content -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
