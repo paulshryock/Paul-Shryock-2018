920b419098f2bc8492bbd85e40a3d6d0edecc211
Paul Shryock <hi@pshry.com>
2017-12-06 16:13:49 -0500

    Add conditional entry link markup

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,13 +12,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
+
+		if ( ! is_singular() ) :
+			echo '<a class="entry-link" href="' . esc_url( get_permalink() ) . '" rel="bookmark"></a>';
+		endif;
+
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif; ?>
+		endif;
 
-		<?php
 		if ( has_post_thumbnail() ) :
 			if ( is_singular() ) :
 				the_post_thumbnail();
