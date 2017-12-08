dace424aa62212347f26980f7403ee28ccd9b196
Paul Shryock <hi@pshry.com>
2017-12-08 12:48:41 -0500

    Update entry content markup and styles

--- template-parts/content.php
+++ template-parts/content.php
@@ -31,27 +31,42 @@
 
 	</header><!-- .entry-header -->
 
-	<?php if ( is_singular() ) : ?>
+	<?php if ( ( ! has_post_thumbnail() ) || is_singular() ) : ?>
 		<section class="entry-content">
 			<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
+			if ( is_singular() ) :
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) );
+			else :
+				the_excerpt( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) );
+			endif;
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'paul-shryock-2018' ),
 				'after'  => '</div>',
 			) ); ?>
-			
+
 		</section><!-- .entry-content -->
 	<?php
 	endif;
