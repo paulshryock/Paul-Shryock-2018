13de5264492819020ef92640cf2401a0faf58a4d
Paul Shryock <hi@pshry.com>
2017-12-11 17:04:56 -0500

    Update entry content markup

--- template-parts/content.php
+++ template-parts/content.php
@@ -31,46 +31,44 @@
 
 	</header><!-- .entry-header -->
 
-	<?php if ( ( ! has_post_thumbnail() ) || is_singular() ) : ?>
-		<section class="entry-content">
-			<?php
-			if ( is_singular() ) :
-				the_content( sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				) );
-			else :
-				the_excerpt( sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				) );
-			endif;
+	<section class="entry-content">
+		<?php
+		if ( is_singular() ) :
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+		else :
+			the_excerpt( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+		endif;
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'paul-shryock-2018' ),
-				'after'  => '</div>',
-			) ); ?>
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'paul-shryock-2018' ),
+			'after'  => '</div>',
+		) ); ?>
 
-		</section><!-- .entry-content -->
+	</section><!-- .entry-content -->
+		
 	<?php
-	endif;
-
 	if ( is_singular() ) : ?>
 	<footer class="entry-footer">
 		<?php paul_shryock_2018_entry_footer(); ?>
