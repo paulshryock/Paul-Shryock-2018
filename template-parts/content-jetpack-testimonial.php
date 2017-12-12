358086f10720b6f6555327cc9f42e5846d837464
Paul Shryock <hi@pshry.com>
2017-12-12 14:59:36 -0500

    Create testimonial content

---
+++ template-parts/content-jetpack-testimonial.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template part for displaying Jetpack portfolio projects
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Paul_Shryock_2018
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif; ?>
+
+	</header><!-- .entry-header -->
+
+	<section class="entry-content">
+		<?php
+		if ( is_single() ) :
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+		else :
+			the_excerpt( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'paul-shryock-2018' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+		endif;
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'paul-shryock-2018' ),
+			'after'  => '</div>',
+		) ); ?>
+
+	</section><!-- .entry-content -->
+		
+	<?php
+	if ( is_singular() ) : ?>
+	<footer class="entry-footer">
+		<?php paul_shryock_2018_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+	<?php endif; ?>
+	
+</article><!-- #post-<?php the_ID(); ?> -->
