6a076c5be7768a95a5adc34f0be16c30db51e2e7
Paul Shryock <hi@pshry.com>
2017-12-13 19:49:35 -0500

    Move template functions

--- functions.php
+++ functions.php
@@ -91,43 +91,6 @@ endif;
 add_action( 'after_setup_theme', 'paul_shryock_2018_setup' );
 
 /**
- * Filter the except length to a modified number of words.
- *
- * @param int $length Excerpt length.
- * @return int (Maybe) modified excerpt length.
- */
-function paul_shryock_2018_custom_excerpt_length( $length ) {
-		return 30;
-}
-add_filter( 'excerpt_length', 'paul_shryock_2018_custom_excerpt_length', 999 );
-
-/**
- * If on a post type archive, use the post type archive title.
- */
-function paul_shryock_2018_archive_title( $title ) {
-	if ( is_post_type_archive() ) {
-		$title = post_type_archive_title( '', false );
-	}
-
-	if ( is_post_type_archive( 'jetpack-portfolio' ) ) {
-		$jetpack_portfolio_title = get_option( 'jetpack_portfolio_title' );
-		if ( isset( $jetpack_portfolio_title ) && '' != $jetpack_portfolio_title ) :
-			$title = esc_html( $jetpack_portfolio_title );
-		endif;
-	}
-
-	if ( is_post_type_archive( 'jetpack-testimonial' ) ) {
-		$jetpack_testimonials = get_theme_mod( 'jetpack_testimonials' );
-		if ( isset( $jetpack_testimonials[ 'page-title' ] ) && '' != $jetpack_testimonials[ 'page-title' ] ) :
-			$title = esc_html( $jetpack_testimonials[ 'page-title' ] );
-		endif;
-	}
-
-	return $title;
-}
-add_filter( 'get_the_archive_title', 'paul_shryock_2018_archive_title' );
-
-/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
