05dfed6dcf85690bd44b859117907ad7fb3091b7
Paul Shryock <hi@pshry.com>
2017-12-13 19:47:37 -0500

    Update post type archive title

--- functions.php
+++ functions.php
@@ -108,6 +108,21 @@ function paul_shryock_2018_archive_title( $title ) {
 	if ( is_post_type_archive() ) {
 		$title = post_type_archive_title( '', false );
 	}
+
+	if ( is_post_type_archive( 'jetpack-portfolio' ) ) {
+		$jetpack_portfolio_title = get_option( 'jetpack_portfolio_title' );
+		if ( isset( $jetpack_portfolio_title ) && '' != $jetpack_portfolio_title ) :
+			$title = esc_html( $jetpack_portfolio_title );
+		endif;
+	}
+
+	if ( is_post_type_archive( 'jetpack-testimonial' ) ) {
+		$jetpack_testimonials = get_theme_mod( 'jetpack_testimonials' );
+		if ( isset( $jetpack_testimonials[ 'page-title' ] ) && '' != $jetpack_testimonials[ 'page-title' ] ) :
+			$title = esc_html( $jetpack_testimonials[ 'page-title' ] );
+		endif;
+	}
+
 	return $title;
 }
 add_filter( 'get_the_archive_title', 'paul_shryock_2018_archive_title' );
