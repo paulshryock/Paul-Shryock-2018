d68ab7fe2f249ee32c76413566063842c7daf875
Paul Shryock <hi@pshry.com>
2017-12-15 06:47:38 -0500

    Add post suffix logic

--- homepage-template.php
+++ homepage-template.php
@@ -56,7 +56,18 @@ get_header(); ?>
 			if ( post_type_exists( 'jetpack-portfolio' ) && $project_query -> have_posts() ) :
 
 				while ( $project_query -> have_posts() ) : $project_query -> the_post();
-					get_template_part( 'template-parts/content', 'jetpack-portfolio' );
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					$post_suffix = get_post_format();
+					if ( is_post_type_archive( 'jetpack-testimonial' ) || is_post_type_archive( 'jetpack-portfolio' ) ) :
+						$post_suffix = get_post_type();
+					endif;
+					get_template_part( 'template-parts/content', $post_suffix );
+				
 				endwhile;
 
 				paul_shryock_2018_paging_nav( $project_query->max_num_pages );
